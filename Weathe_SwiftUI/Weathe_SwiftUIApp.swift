dbf4e57e9361e6e2467a8bf6df2b000ef14049b8
terentyev-va <terentyev.va@yandex.ru>
2022-08-10 07:37:28 +0300

    Initial Commit

---
+++ Weathe_SwiftUI/Weathe_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weathe_SwiftUIApp.swift
+//  Weathe_SwiftUI
+//
+//  Created by Вячеслав Терентьев on 10.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Weathe_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
